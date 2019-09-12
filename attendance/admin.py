4002f49dde0031de7fef7b40cde21a89464aeea7
NamelessOgya <hakuginnnohana@gmail.com>
2019-09-12 09:11:11 +0900

    import objects to admin site

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from .models import Team, Player, Match, Registered
 # Register your models here.
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Match)
+admin.site.register(Registered)
