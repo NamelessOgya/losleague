f2cafbf08f03a7a99a6c6df3baa0fef61fe8c602
NamelessOgya <hakuginnnohana@gmail.com>
2019-09-25 17:04:03 +0900

    revise spel miss

--- los/settings.py
+++ los/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -189,7 +189,8 @@ else:
     DATABASES = {
         'default': dj_database_url.config()
     }
-    ALLOWED_HOSTS = ['*']import dj_database_url
+    ALLOWED_HOSTS = ['*']
+    import dj_database_url
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     db_from_env = dj_database_url.config()
     DATABASES = {
