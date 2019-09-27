8da83c63edf9d1e6732db6ef98f61b7a9553779e
NamelessOgya <hakuginnnohana@gmail.com>
2019-09-27 13:57:44 +0900

    SECRET_key again

--- los/settings.py
+++ los/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '79qzfqu5fx(v)wur$-%rutqql@hj48uu^*j44@=zo_)ovtf0*q'
 # SECURITY WARNING: don't run with debug turned on in productio
 
 
